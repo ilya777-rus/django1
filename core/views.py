b41b6a46f457cad2d47a1c36b664ad836f794b2b
ilya777-rus <nickolaeff.ilha@yandex.ru>
2024-05-08 23:57:04 +0500

    shablone_dj

--- core/views.py
+++ core/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
 
+from . import models
+
+
 def index(request):
-    return HttpResponse(datetime.today().strftime("%Y-%m-%d %H:%M:%S"))
+    posts = models.Post.objects.all()
+
+    return render(request, 'core/index.html', context={'posts':posts, "title":"Список постов"})
+    # return HttpResponse(datetime.today().strftime("%Y-%m-%d %H:%M:%S"))
 

d9f2db321e6f7c02f5a39aacb208d7a01eb86abf
ilya777-rus <nickolaeff.ilha@yandex.ru>
2024-05-11 00:59:35 +0500

    dj3

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -10,6 +11,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'category_id':self.pk})
 
 
 class Post(models.Model):
@@ -28,3 +32,4 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
--- core/views.py
+++ core/views.py
@@ -7,7 +7,22 @@ from . import models
 
 def index(request):
     posts = models.Post.objects.all()
-
-    return render(request, 'core/index.html', context={'posts':posts, "title":"Список постов"})
+    categories = models.Category.objects.all()
+    context = {
+               'posts': posts,
+               'categories':categories,
+               "title": "Список постов",
+               }
+    return render(request, 'core/index.html', context=context)
     # return HttpResponse(datetime.today().strftime("%Y-%m-%d %H:%M:%S"))
 
+def get_category(request, category_id):
+    posts = models.Post.objects.filter(category=category_id)
+    categories = models.Category.objects.all()
+    category = models.Category.objects.get(pk=category_id)
+    context = {
+               'posts': posts,
+               'categories':categories,
+               "category": category,
+               }
+    return render(request, 'core/category.html', context=context)
